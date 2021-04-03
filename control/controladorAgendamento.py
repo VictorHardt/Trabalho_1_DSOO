97c261d66c54393a26cc5512c064b5da5b235411
Marcelo Schuler <marceloschuler1234@gmail.com>
2021-04-03 18:26:16 -0300

    Finalização da classe Agendamento

--- control/controladorAgendamento.py
+++ control/controladorAgendamento.py
@@ -37,7 +37,7 @@ class ControladorAgendamento:
         hora = dados_agendamento["hora"]
         vacina = self.__controlador_vacina.retorna_vacina_para_agendamento()
         if paciente is None:
-            self.__tela.paciente_nao_existe_error(dados_agendamento["cpf"])
+            self.__tela.paciente_nao_existe_error(dados_agendamento["cpf"]) 
         elif enfermeiro is None:
             self.__tela.enfermeiro_nao_existe_error(dados_agendamento["nome"])
         elif vacina is None:
@@ -48,13 +48,67 @@ class ControladorAgendamento:
         
 
     def checa_agendamento(self):
-        pass
+
+        cpf = self.__tela.recebe_cpf()
+        agendamento = self.retorna_agendamento(cpf)
+        if agendamento:
+            self.__tela.mostra_agendamento(agendamento.paciente.nome, agendamento.enfermeiro.nome, agendamento.data.year, agendamento.data.month, agendamento.data.day, agendamento.vacina.fabricante)
+        else:
+            self.__tela.nao_ha_agendamento(cpf)
+
 
     def remove_agendamento(self):
-        pass
+        
+        cpf = self.__tela.recebe_cpf()
+        agendamento = self.retorna_agendamento(cpf)
+        if agendamento:
+            self.__agendamentos.remove(agendamento)
+            self.__tela.removeu_agendamento(None)
+        else:
+            self.__tela.removeu_agendamento(cpf)
 
     def altera_agendamento(self):
-        pass
+        
+        cpf = self.__tela.recebe_cpf()
+        agendamento = self.retorna_agendamento(cpf)
+        if agendamento:          
+            opcao_escolhida = self.__tela.opcao_para_alteracao()
+            if opcao_escolhida == 1: #muda a data
+                nova_data = self.__tela.escolher_data()
+                agendamento.data(nova_data)
+                self.__tela.alterado()
+            elif opcao_escolhida == 2: #muda a hora
+                nova_hora = self.__tela.escolher_hora()
+                agendamento.hora(nova_hora)
+                self.__tela.alterado()
+            elif opcao_escolhida == 3: #muda o enfermeiro
+                nome_enfermeiro = self.__tela.escolher_enfermeiro()
+                enfermeiro = self.__controlador_enfermeiro.retorna_enfermeiro(nome_enfermeiro)
+                if enfermeiro:
+                    agendamento.enfermeiro(enfermeiro)
+                    self.__tela.alterado()
+                else:
+                    self.__tela.enfermeiro_nao_existe_error(nome_enfermeiro)
+            else: #muda o paciente
+                cpf_paciente = self.__tela.escolher_paciente()
+                paciente = self.__controlador_paciente.retorna_paciente(cpf_paciente)
+                if paciente:
+                    agendamento.paciente(paciente)
+                    self.__tela.alterado()
+                else:
+                    self.__tela.paciente_nao_existe_error(cpf_paciente)
+        else:
+            self.__tela.nao_ha_agendamento(cpf)
+
+    def retorna_agendamento(self, cpf):
+
+        agendamento = None
+        i = 0
+        while agendamento == None and i<len(self.__agendamentos):
+            if cpf == self.__agendamentos[i].paciente.cpf:
+                agendamento = self.__agendamentos[i]
+            i += 1   
+        return agendamento
 
     def retorna(self):
         self.__continuar = False
--- view/telaAgendamento.py
+++ view/telaAgendamento.py
@@ -14,16 +14,63 @@ class TelaAgendamento(AbstractTela):
         print("1 : Novo Agendamento")
         print("2 : Checa Agendamento")
         print("3 : Remove Agendamento")
-        print("4 : Checa Agendamento")
-        print("5 : Altera Agendamento")
+        print("4 : Altera Agendamento")
         print("0 : Retornar")
         print("")
 
         return self.ler_numero([1,2,3,4,0])
 
     def recebe_dados_agendamento(self):
-        cpf_paciente = self.ler_string("Digite seu cpf: ")
-        nome_enfermeiro = self.ler_string("Digite o nome do enfermeiro: ")
+
+        cpf_paciente = self.recebe_cpf()
+        nome_enfermeiro = self.escolher_enfermeiro()
+        data = self.escolher_data()
+        hora = self.escolher_hora()
+        dados_agendamento = {"cpf": cpf_paciente, "nome_enfermeiro": nome_enfermeiro, "data": data, "hora": hora}
+        return dados_agendamento
+
+    def recebe_cpf(self):
+        return self.ler_string("Digite seu cpf: ")
+
+    def paciente_nao_existe_error(self, cpf):
+        print("")
+        print("Não existe nenhum paciente cadastrado com o cpf {}, realize o cadastro do paciente antes do agendamento.".format(cpf))
+
+    def enfermeiro_nao_existe_error(self, nome):
+        print("")
+        print("Não existe nenhum enfermeiro cadastrado com o nome {}, realize o cadastro do enfermeiro antes do agendamento.".format(nome)) 
+
+    def sem_estoque_de_vacina_error(self):
+        print("")
+        print("Não há doses suficientes de vacina para o agendamento.")
+
+    def mostra_agendamento(self, paciente_nome, enfermeiro_nome, ano, mes, dia, fabricante):
+        print("")
+        print("{}, você tem um agendamento para o dia {}/{}/{} com o enfermeiro {}, para ser vacinado com a vacina {}".format(paciente_nome, enfermeiro_nome, ano, mes, dia, fabricante))
+
+    def nao_ha_agendamento(self, cpf):
+        print("")
+        print("Não há nenhum agendamento com o cpf {}.".format(cpf))
+
+    def removeu_agendamento(self, cpf):
+
+        if cpf:
+            print(f"Não há nenhum agendamento com o cpf {cpf}")
+        elif cpf is None:
+            print("Seu agendamento foi removido com sucesso!")
+
+    def opcao_para_alteracao(self):
+
+        print("O que você quer alterar no agendamento?")
+        print("")
+        print("1 : Data")
+        print("2 : Hora")
+        print("3 : Enfermeiro")
+        print("4 : Paciente")
+
+        return self.ler_numero([1,2,3,4])
+
+    def escolher_data(self):
         data = None
         while not isinstance(data, date):
             try:
@@ -33,21 +80,17 @@ class TelaAgendamento(AbstractTela):
                 data = date(ano, mes, dia)
             except Exception:
                 print("A data não é válida!")
-        hora = self.ler_numero([8,9,10,11,12,13,14,15,16,17,18], "Escolha um horário entre 8 e 16 horas: ")
-        dados_agendamento = {"cpf": cpf_paciente, "nome_enfermeiro": nome_enfermeiro, "data": data, "hora": hora}
-        return dados_agendamento
+        return data
 
-    def paciente_nao_existe_error(self, cpf):
-        print("")
-        print("Não existe nenhum paciente cadastrado com o cpf {}, realize o cadastro do paciente antes do agendamento.".format(cpf))
-        print("")
+    def escolher_enfermeiro(self):
+        return self.ler_string("Digite o nome do enfermeiro: ")
 
-    def enfermeiro_nao_existe_error(self, nome):
-        print("")
-        print("Não existe nenhum enfermeiro cadastrado com o nome {}, realize o cadastro do enfermeiro antes do agendamento.".format(nome)) 
-        print("")
+    def escolher_hora(self):
+        return self.ler_numero([8,9,10,11,12,13,14,15,16,17,18], "Escolha um horário entre 8 e 18 horas: ")
+    
+    def escolher_paciente(self):
+        return self.ler_string("Digite o cpf do novo paciente: ")
 
-    def sem_estoque_de_vacina_error(self):
+    def alterado(self):
         print("")
-        print("Não há doses suficientes de vacina para o agendamento.")
-        print("")
+        print("Seu agendamento foi alterado com sucesso!")
